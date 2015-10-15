243a255ca3b7ad38eeeeb8e62a570a5a819b11d8
Camille Bovin <camille.bovin23@gmail.com>
2015-10-15 09:19:46 +0200

    Ajout des boutons mois/semaine/jour du calendrier

---
+++ public/js.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    
+
+    $('#calendar').fullCalendar({
+        header: {
+        left: 'prevYear,nextYear, prev,next',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+    }
+    })
+
+});
+
