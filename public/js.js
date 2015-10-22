7cebdf75d3b577dab5838a23e8c0f56087cc6fb9
Camille Bovin <camille.bovin23@gmail.com>
2015-10-22 09:25:35 +0200

    Modification du nom VT.html en index.html

--- public/js.js
+++ public/js.js
@@ -11,3 +11,27 @@ $(document).ready(function() {
 
 });
 
+$(function () {
+ //console.log(req.session.cookie);
+
+
+ $.ajax({url: "/matiere", success: function(result){ // putting "admin" need to be the pseudo log
+ 	// document.cookie ="username="+result[0].userName;
+ 	  //alert(document.cookie);
+              for (i = 0; i< result.length; i++){
+              console.log(result[i].nom);
+              //console.log(result[i].diagrammeName);
+              //   $("tbody:nth-child(4)")
+           document.getElementById('CreeEnseignement').getElementsByTagName('select')[0].innerHTML += //"<tr>"+
+		//"<th>"+"Edition"+"</th>"+
+		//"<th>"+"Suppression"+"</th>"+
+		//"<th>"+"N°"+"</th>"+
+		//"<th>"+"Diagrammes"+"</th>"+
+	//"</tr>"+
+	"<option>"+result[i]._nom+"<option>";
+              }
+         
+        }});	
+
+
+    });
