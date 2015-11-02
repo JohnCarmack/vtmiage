e6e2c580ca77636c1d9a03539e71c31241bb9ef5
Camille Bovin <camille.bovin23@gmail.com>
2015-11-02 12:50:17 +0100

    Bug : Nom Enseignement

--- public/js.js
+++ public/js.js
@@ -57,13 +57,13 @@ function creerMatiere(){
 }
 
 function NomEnseignement(){
-	if(document.getElementById('Exam').checked==true && document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value != ""){
+	if(document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value != "" && document.getElementById('Exam').checked==true){
 			document.getElementById('nom_enseignement').value= document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value + " - Exam";
 		}
-	if(document.getElementById('CM').checked==true && document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value != ""){
+	if(document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value != "" && document.getElementById('CM').checked==true ){
 		document.getElementById('nom_enseignement').value= document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value + " - CM";
 	}
-	if(document.getElementById('TD').checked==true && document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value != ""){
+	if(document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value != "" && document.getElementById('TD').checked==true){
 		document.getElementById('nom_enseignement').value= document.getElementById('CreerEnseignement').getElementsByTagName('select')[0].value + " - TD";
 	}
 }
